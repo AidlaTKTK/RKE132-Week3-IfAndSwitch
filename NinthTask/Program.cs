ddc56a67d14a1831b55bbd051202813364439b1c
Markus Aidla <markus.aidla@tktk.ee>
2024-09-15 20:53:04 +0300

    Add week 3 code

---
+++ NinthTask/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Tegin nii, et hinnet saab kirjutada suure ja väikse tähega, et programm oleks lollikindlam. 
+Ei tea, kas ülesandes pidi suur täht olema ainuke viis, kuidas hinnet sisestada,
+aga seda oleks lihtne parandada, eemaldades ".ToLower()" kaheksandalt realt ja muutes if-lausetes [väikse tähe suureks]. <- kirjutasin testis kogemata valesti, märkasin pärast saatmist
+*/
+
+Console.WriteLine("Mis hinde said?");
+char hinne = Convert.ToChar(Console.ReadLine().ToLower());
+
+if (hinne == 'a')
+{
+    Console.WriteLine("Suurepärane!");
+}
+else if (hinne == 'b')
+{
+    Console.WriteLine("Väga hea!");
+}
+else if (hinne == 'c')
+{
+    Console.WriteLine("Hea!");
+}
+else if (hinne == 'd')
+{
+    Console.WriteLine("Rahuldav");
+}
+else if (hinne == 'e')
+{
+    Console.WriteLine("Kasin");
+}
+else if (hinne == 'f')
+{
+    Console.WriteLine("Puudulik");
+}
+else
+{
+    Console.WriteLine("Vale väärtus");
+}
---
+++ TenthTask/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Tegin nii, et hinnet saab kirjutada suure ja väikse tähega, et programm oleks lollikindlam. 
+Ei tea, kas ülesandes pidi suur täht olema ainuke viis, kuidas hinnet sisestada,
+aga seda oleks lihtne parandada, eemaldades ".ToLower()" kaheksandalt realt ja muutes switch lülitise case'is [väikse tähe suureks]. <- kirjutasin testis kogemata valesti, märkasin pärast saatmist
+*/
+
+Console.WriteLine("Mis hinde said?");
+char hinne = Convert.ToChar(Console.ReadLine().ToLower());
+
+switch(hinne)
+{
+    case 'a':
+        Console.WriteLine("Suurepärane!");
+        break;
+    case 'b':
+        Console.WriteLine("Väga hea!");
+        break;
+    case 'c':
+        Console.WriteLine("Hea!");
+        break;
+    case 'd':
+        Console.WriteLine("Rahuldav");
+        break;
+    case 'e':
+        Console.WriteLine("Kasin");
+        break;
+    case 'f':
+        Console.WriteLine("Puudulik");
+        break;
+    default:
+        Console.WriteLine("Vale väärtus");
+        break;
+}
